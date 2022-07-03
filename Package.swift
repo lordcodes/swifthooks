72c0b7ccf168a407dcb374dfb1d8c6439b87a5dc
Andrew Lord <andrew@lordcodes.com>
2022-07-03 11:16:53 +0100

    Output SwiftHooksKit as a framework

--- Package.swift
+++ Package.swift
@@ -6,14 +6,9 @@ let package = Package(
     name: "swifthooks",
     platforms: [.macOS(.v11)],
     products: [
-        .executable(
-            name: "tuist-hooks",
-            targets: ["SwiftHooksCLI"]
-        ),
-        .executable(
-            name: "swifthooks",
-            targets: ["SwiftHooksCLI"]
-        )
+        .executable(name: "tuist-hooks", targets: ["SwiftHooksCLI"]),
+        .executable(name: "swifthooks", targets: ["SwiftHooksCLI"]),
+        .library(name: "SwiftHooksKit", targets: ["SwiftHooksKit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/JohnSundell/Files", exact: "4.2.0"),
