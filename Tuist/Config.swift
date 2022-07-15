daa234b4e7a182ce80c8a5823b232b43b518c2b0
Andrew Lord <andrew@lordcodes.com>
2022-07-15 21:33:03 +0100

    Latest SwiftFormat plugin

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -3,6 +3,6 @@ import ProjectDescription
 let config = Config(
     compatibleXcodeVersions: .upToNextMinor("13.4"),
     plugins: [
-        .git(url: "https://github.com/lordcodes/tuist-plugin-swiftformat", tag: "0.1.0"),
+        .git(url: "https://github.com/lordcodes/swiftformat-tuist", tag: "v0.3.0"),
     ]
 )
